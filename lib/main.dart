5e626031f69e0956a56cf6fe7d21a7ae09f5504f
thanchanok <fang181047@gmail.com>
2026-06-06 14:15:15 +0700

    steb 3: Container Widget

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,20 @@ class Home extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return Text("Hello World 2");
+    return Center(
+      child: Container(
+        color : const Color.fromARGB(255, 15, 93, 166),
+        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        padding: EdgeInsets.all(50),
+        child: Text(
+          "Hello Word 3",
+          style: TextStyle(
+            fontSize: 30,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ), 
+    );
   }
 }
